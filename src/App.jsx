d01aea371a20ae38f8fb9a3bc1111186b514a3e6
cruz-welandi <cruzwelandi@gmail.com>
2023-11-07 15:16:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <p className='text-xl font-bold'>
+        hello
+      </p>
+    </>
+  )
+}
+
+export default App
