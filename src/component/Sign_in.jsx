1849c0828539ad54348842298da89327912afdce
cruz-welandi <cruzwelandi@gmail.com>
2023-11-08 06:31:12 +0100

    ajout sign up

--- src/component/Sign_in.jsx
+++ src/component/Sign_in.jsx
@@ -25,16 +25,16 @@ function Sign_in (){
                         <label className="font-bold">
                             Email
                         </label>
-                        <input type="email" className="bg-[#e5e7eb] h-[50px] rounded-lg"/>
+                        <input type="email" className="bg-[#e5e7eb] h-[60px] rounded-lg"/>
                     </div>
                     <div className="flex flex-col gap-y-2">
                         <label className="font-bold flex justify-between">
                             Mot de passe
                             <Link to={'/Forgot-your-password'} className="text-[#4338ca]">Mot de passe oublié</Link>
                         </label>
-                        <input type="password" className="bg-[#e5e7eb] h-[50px] rounded-lg"/>
+                        <input type="password" className="bg-[#e5e7eb] h-[60px] rounded-lg"/>
                     </div>
-                    <div className="flex gap-x-10">
+                    <div className="flex gap-x-8">
                         <input type="checkbox" className="default:bg-[#e5e7eb] w-5"/>
                         <label className="font-bold">Se souvenir de moi</label>
                     </div>
@@ -44,7 +44,7 @@ function Sign_in (){
                     </button>
                 </form>
 
-                <div className="flex gap-x-3 mt-2.5">
+                <div className="flex gap-x-3 mt-2.5 justify-center">
                     Vous n'avez pas un compte? 
                     <Link to={'/Sign-up'} className="text-[#4338ca]">
                         Créer un compte c'est gratuit
--- src/component/Sign_up.jsx
+++ src/component/Sign_up.jsx
@@ -1,9 +1,71 @@
-
+import { Link } from "react-router-dom";
 
 function Sign_up (){
 
     return (
-        <></>
+        <>
+            <div className="flex">
+                <div className="w-1/2 flex flex-col gap-y-5 py-5 px-24 ">
+                    <div className="flex flex-col gap-y-3">
+                        <div className="p-4 bg-[#e0e7ff] w-14 rounded-lg text-[#4338ca] font-bold">
+                            E41
+                        </div>
+                        <div className="flex flex-col gap-y-3">
+                            <h1 className="text-3xl font-bold">
+                                Créer un compte
+                            </h1>
+                            <p className="text-xl ">
+                                Ce n'est pas difficile, il suffit de saisir quelques données
+                            </p>
+                        </div>
+                    </div>
+            
+                    <form className="flex flex-col gap-y-3">
+                        <div  className="flex flex-col gap-y-2">
+                            <label  className="font-bold">
+                                Nom complet
+                            </label>
+                            <div className="flex gap-x-6">
+                                <input type="text" className="bg-[#e5e7eb] h-[50px] w-[250px] rounded-lg pl-6 placeholder:text-black" placeholder="NOM"/>
+                                <input type="text" className="bg-[#e5e7eb] h-[50px]  w-[250px] rounded-lg pl-6 placeholder:text-black" placeholder="PRENOM"/>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-y-2">
+                            <label className="font-bold">
+                                Email
+                            </label>
+                            <input type="email" className="bg-[#e5e7eb] h-[50px] rounded-lg pl-6 placeholder:text-black" placeholder="aurelius@rocketmail.com"/>
+                        </div>
+
+                        <div className="flex flex-col gap-y-2">
+                            <label className="font-bold">
+                                Mot de passe
+                            </label>
+                            <input type="password" className="bg-[#e5e7eb] h-[50px] rounded-lg pl-6 placeholder:text-6xl placeholder:font-blod placeholder:text-black" placeholder=".................."/>
+                        </div>
+
+                        <p>
+                            En vous Inscrivant, vous acceptez les conditions d'utilisation et la politique de confidentialité de 
+                            Namanyajugabelajar.io                        
+                        </p>
+
+                        <button className="bg-[#4338ca] p-3 rounded-lg text-white font-bold drop-shadow-2xl">
+                            S'incrire
+                        </button>
+
+                    </form>
+                    <div className="flex gap-x-3 justify-center">
+                            Vous avez déjà un compte ?
+                            <Link to={'/Sign-in'} className="text-[#4338ca]">Connetez vous</Link>
+                    </div>
+                </div>
+                <div className="w-1/2">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </>
     )
 }
 
